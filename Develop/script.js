ecd3503a21fed5e8360ecc897566c243217620ae
Jessica Gonzalez <jessgonz0779@gmail.com>
2020-10-17 21:41:58 -0400

    adjusted button on CSS file to mark it red

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,115 @@
-// Assignment Code
 var generatePasswordButton = document.getElementById("generate");
 var copyToClipBoardButton = document.getElementById("copytoclipboard");
 
+var onlyUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var onlyLower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var onlySpecial = ['#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','['];
+var onlyNumeric = [0,1,2,3,4,5,6,7,8,9];
 
-// Write password to the #password input
-function password_generate(lower, upper, number, symbol, length) {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+function generateOptions() {
+
+  
+    var length = parseInt(prompt("How many characters would you like your password to contain?"));
+
+    // alerts added for passwork requirementd
+
+    if (length < 8) {
+        alert('The password must be at least 8 characters!');
+        return;
+    }
+    if (length > 128) {
+        alert('The password must be less than 128 characters!');
+        return;
+    }
+
+    //prompt user for special characters
+    var shouldIncludeSpecialCharacters = confirm("Do you want to include special characters?");
+    
+    //prompt user for numeric characters
+    var shouldIncludeNumeric = confirm("Do you want to include numeric characters?");
+    
+    //prompt user for lowercase characters
+    var shouldIncludeLowercase = confirm("Do you want to include lowercase characters?");
+    
+    //prompt user for uppercase characters
+    var shouldIncludeUppercase = confirm("Do you want to include uppercase characters?");
+    
+    if (!shouldIncludeLowercase && !shouldIncludeUppercase && !shouldIncludeNumeric && !shouldIncludeSpecialCharacters) {
+        alert("Your password must contain at least one special, numeric, lowercase, or uppercase character");
+        return;
+    }
+    //alerts end
+
+    var questionOptions = {
+        length: length,
+        specialCharacters: shouldIncludeSpecialCharacters,
+        numeric: shouldIncludeNumeric,
+        lowerCase: shouldIncludeLowercase,
+        upperCase: shouldIncludeUppercase
+    }
+
+    return questionOptions;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-object.addEventListener("click", myScript);
+function generatePassword() {
 
+    var options = generateOptions();
+    console.log(options)
+
+    var passwordPool = [];
+    console.log(passwordPool)
+
+    if (options.specialCharacters) {
+        for (i = 0; i < onlySpecial.length; ++i) {
+            passwordPool.push(onlySpecial[i]);
+        }
+    } 
+    if (options.numeric) {
+        for (i = 0; i < onlyNumeric.length; ++i) {
+        passwordPool.push(onlyNumeric[i]);
+        }
+    }
+    if (options.lowerCase) {
+        for (i = 0; i < onlyLower.length; ++i) {
+        passwordPool.push(onlyLower[i]);
+        }
+    }
+    if (options.upperCase) {
+        for (i = 0; i < onlyUpper.length; ++i) {
+        passwordPool.push(onlyUpper[i]);
+
+      }
+    }
+
+    var finalPassword = [];
+
+    for (let i = 0; i < options.length; ++i) {
+        var randomPicker = Math.floor(Math.random() * Math.floor(passwordPool.length));
+         finalPassword.push(passwordPool[randomPicker])
+    }
+
+    console.log(finalPassword)
+
+    var superFinal = finalPassword.join('');
+    console.log(superFinal)
+
+    document.getElementById("display").textContent = superFinal;
+}
+
+
+    var password = "";
+
+
+function copytoClipboard() {
+
+    document.getElementById("git display").select();
+
+    document.execCommand("Copy");
+
+    alert("Your password has now been copied to the clipboard");
+
+}
+
+generatePasswordButton.addEventListener('click', generatePassword);
